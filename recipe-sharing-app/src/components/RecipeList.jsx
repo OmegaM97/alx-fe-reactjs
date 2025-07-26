b9c0abab70d8378dea26110f4adcf8b0ffa1be08
Omega Melese <omegamelese68@gmail.com>
2025-07-26 16:26:48 +0300

    restructure the files

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,4 @@
-import { useRecipeStore } from '../store/recipeStore';
+import { useRecipeStore } from './recipeStore.js';
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
