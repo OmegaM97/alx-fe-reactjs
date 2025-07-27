a7bc378ce251a2b0075400b59a6fcbe7acb8b063
Omega Melese <omegamelese68@gmail.com>
2025-07-27 10:25:30 +0300

    add filtering feature

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,18 +1,44 @@
 import { create } from 'zustand';
 
-export const useRecipeStore = create((set) => ({
+export const useRecipeStore = create((set, get) => ({
   recipes: [],
+  searchTerm: '',
+  filteredRecipes: [],
   addRecipe: (newRecipe) =>
-    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+    set((state) => {
+      const updatedRecipes = [...state.recipes, newRecipe];
+      return {
+        recipes: updatedRecipes,
+        filteredRecipes: filter(updatedRecipes, state.searchTerm),
+      };
+    }),
   deleteRecipe: (id) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
-    })),
+    set((state) => {
+      const updatedRecipes = state.recipes.filter((r) => r.id !== id);
+      return {
+        recipes: updatedRecipes,
+        filteredRecipes: filter(updatedRecipes, state.searchTerm),
+      };
+    }),
   updateRecipe: (updatedRecipe) =>
-    set((state) => ({
-      recipes: state.recipes.map((recipe) =>
+    set((state) => {
+      const updatedRecipes = state.recipes.map((recipe) =>
         recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-      ),
+      );
+      return {
+        recipes: updatedRecipes,
+        filteredRecipes: filter(updatedRecipes, state.searchTerm),
+      };
+    }),
+  setSearchTerm: (term) =>
+    set((state) => ({
+      searchTerm: term,
+      filteredRecipes: filter(state.recipes, term),
     })),
-  setRecipes: (recipes) => set({ recipes }),
 }));
+
+// Utility function to filter recipes
+const filter = (recipes, term) =>
+  recipes.filter((recipe) =>
+    recipe.title.toLowerCase().includes(term.toLowerCase())
+  );
