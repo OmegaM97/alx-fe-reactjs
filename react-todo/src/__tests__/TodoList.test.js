70125ed5bea48dc71274d181cff8e465a70cd968
Omega Melese <omegamelese68@gmail.com>
2025-08-28 11:54:05 +0300

    add test file in the test directory too

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,32 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TodoList from "./TodoList";
+
+test("renders initial todos", () => {
+  render(<TodoList />);
+  expect(screen.getByText("Learn React")).toBeInTheDocument();
+  expect(screen.getByText("Build a Todo App")).toBeInTheDocument();
+});
+
+test("adds a new todo", () => {
+  render(<TodoList />);
+  fireEvent.change(screen.getByPlaceholderText("Add todo..."), {
+    target: { value: "New Task" },
+  });
+  fireEvent.click(screen.getByText("Add"));
+  expect(screen.getByText("New Task")).toBeInTheDocument();
+});
+
+test("toggles todo completion", () => {
+  render(<TodoList />);
+  const todo = screen.getByText("Learn React");
+  fireEvent.click(todo);
+  expect(todo).toHaveStyle("text-decoration: line-through");
+});
+
+test("deletes a todo", () => {
+  render(<TodoList />);
+  const todo = screen.getByText("Learn React");
+  fireEvent.click(screen.getAllByText("Delete")[0]); // delete first todo
+  expect(todo).not.toBeInTheDocument();
+});
